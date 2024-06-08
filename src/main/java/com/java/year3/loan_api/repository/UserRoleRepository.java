1760a5377f507ddd40cce8cb1cc1e997caa63596
soda.kroes <sodait25@gmail.com>
2024-06-08 14:15:21 +0700

    soda update code on function jwt generate token with role

--- src/main/java/com/java/year3/loan_api/repository/UserRoleRepository.java
+++ src/main/java/com/java/year3/loan_api/repository/UserRoleRepository.java
@@ -4,6 +4,7 @@ import com.java.year3.loan_api.entity.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
     boolean existsByName(String name);
